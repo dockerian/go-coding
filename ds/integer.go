4ef26a4891030680baf48f3ca55cbfe1b377efae
Jason Zhu <jason.zhuyx@gmail.com>
2016-09-15 08:35:51 -0700

    fixed ReverseInt64 on issue of int64 overflow

--- ds/integer.go
+++ ds/integer.go
@@ -2,6 +2,8 @@ package ds
 
 import (
 	"fmt"
+
+	u "github.com/dockerian/go-coding/utils"
 )
 
 // Compare returns 0 if a == b; 1 if a > b; or -1 if a < b
@@ -13,6 +15,11 @@ func Compare(a, b uint64) int {
 		return -1
 	}
 	return 0
+}
+
+// EqualSign checks if a and b are both same signed (positive or negative)
+func EqualSign(a, b int64) bool {
+	return a >= 0 && b >= 0 || a < 0 && b < 0
 }
 
 // MultiplyUint64 gets muliplication of two integers
@@ -35,11 +42,21 @@ func SumUint64(a, b uint64) (uint64, error) {
 
 // ReverseInt64 reverses a decimal integer
 func ReverseInt64(number int64) int64 {
-	var reversed int64
+	var result int64
 	for number != 0 {
-		reversed = reversed*10 + number%10
+		var unit = number % 10
+		var test = result*10 + unit
+		// u.Debug("result= %v, test= %v, unit= %v\n", result, test, unit)
+		// Note: maximium int64 + 1 => minimium int64 (negative)
+		//       minimium int64 - 1 => maximium int64 (positive)
+		if (test-unit)/10 != result || result != 0 && !EqualSign(test, result) {
+			return 0
+		}
+		// u.Debug("result= %v, test= %v, number= %v\n", result, test, number)
+		result = test
 		number /= 10
 	}
 
-	return reversed
+	u.Debug("number= %v, result= %v\n\n", number, result)
+	return result
 }
--- ds/integer_test.go
+++ ds/integer_test.go
@@ -16,12 +16,23 @@ type ReverseInt64TestCase struct {
 }
 
 // TestReverseInt64 tests ReverseInt function
+// Note: integer range
+//       int16: -32,768 to 32,767
+//       int32: -2,147,483,648 to 2,147,483,647
+//       int64: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// Note: maximium int64 + 1 => minimium int64 (negative)
+//       minimium int64 - 1 => maximium int64 (positive)
 func TestReverseInt64(t *testing.T) {
 	var tests = []ReverseInt64TestCase{
 		{121, 121},
 		{12345, 54321},
 		{-1234567, -7654321},
 		{31415926, 62951413},
+		{0x7FFFFFFFFFFFFFFF, 7085774586302733229},
+		{7085774586302733229, 9223372036854775807},
+		{8085774586302733229, 0},
+		{-8085774586302733229, -9223372036854775808},
+		{-8085774586302733230, -323372036854775808},
 		{-666, -666},
 		{777, 777},
 		{9112000, 2119},
@@ -31,7 +42,7 @@ func TestReverseInt64(t *testing.T) {
 
 	for index, test := range tests {
 		var val = ReverseInt64(test.Number)
-		var msg = fmt.Sprintf("expecting %v == %v", val, test.Reversed)
+		var msg = fmt.Sprintf("expecting %v => %v", test.Number, test.Reversed)
 		t.Logf("Test %v: %v\n", index+1, msg)
 		assert.Equal(t, test.Reversed, val, msg)
 	}
