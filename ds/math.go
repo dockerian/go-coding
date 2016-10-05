eb9480ed45d17f9a26adee62245b2f398c017259
Jason Zhu <jason.zhuyx@gmail.com>
2016-10-05 15:39:54 -0700

    added RandRange* and Sqrt functions in math.go

--- ds/math.go
+++ ds/math.go
@@ -1,7 +1,16 @@
 package ds
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const (
+	// MathPrecision set precision for math functions
+	MathPrecision = 0.00000000000001
 )
 
 // LogBaseX caculates base log of x
@@ -14,4 +23,58 @@ func LogBaseX(base int32, x float64) float64 {
 		return math.Log10(x)
 	}
 	return math.Log2(x) / math.Log2(float64(base))
+}
+
+// NearEqual compares a and b in range of precision
+func NearEqual(a, b, precision float64) bool {
+	diff := math.Abs(a - b)
+	return diff < precision
+}
+
+// RandRangeFloat64 returns a pseudo-random float64 in [min,max)
+func RandRangeFloat64(min, max float64) float64 {
+	if max < min {
+		max, min = min, max
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Float64()*(max-min) + min
+}
+
+// RandRangeInt64 returns a pseudo-random int64 in [min,max)
+func RandRangeInt64(min, max int64) int64 {
+	if max < min {
+		max, min = min, max
+	}
+	if max-min == 0 {
+		return min
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int63n(max-min) + min
+}
+
+// Sqrt calculates square root of x (to demonstrate float point comparison)
+func Sqrt(x float64) float64 {
+	switch {
+	case x == 0 || math.IsNaN(x) || math.IsInf(x, 1):
+		return x
+	case x < 0:
+		return math.NaN()
+	}
+
+	const BEGIN, PRECISION = 1.0, MathPrecision
+	var result, temp = BEGIN, float64(0.0)
+
+	for {
+		temp = result - ((result*result - x) / (2 * result))
+		if diff := math.Abs(result - temp); diff < PRECISION {
+			break
+		} else {
+			result = temp
+		}
+	}
+
+	resultString := fmt.Sprintf("%.20f", result)
+	result, _ = strconv.ParseFloat(resultString, 20)
+
+	return result
 }
--- ds/math_test.go
+++ ds/math_test.go
@@ -12,7 +12,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestLogBaseX tests LogBaseX
+// RandRangeFloat64TestCase struct
+type RandRangeFloat64TestCase struct {
+	min, max float64
+}
+
+// RandRangeInt64TestCase
+type RandRangeInt64TestCase struct {
+	min, max int64
+}
+
+// TestLogBaseX tests LogBaseX functions
 func TestLogBaseX(t *testing.T) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
@@ -32,5 +42,71 @@ func TestLogBaseX(t *testing.T) {
 		message := fmt.Sprintf("%v\n%v\n%v", msg1, msg2, msg3)
 		t.Logf("Test %2d: %v\n\n", i, message)
 		assert.True(t, test, message)
+	}
+}
+
+// TestRandRange tests both RandRangeFloat64 and RandRangeInt64 functions
+func TestRandRange(t *testing.T) {
+	tests := []RandRangeFloat64TestCase{
+		{0.0, 0.0},
+		{0.0, 0.1},
+		{2.0, 100.5},
+		{-3.3, 10.1},
+		{-3.0, -10.5},
+		{99.9, -10.7},
+		{99.9, 99.9},
+		{123.456789, 98.7654321},
+		{777.777, 0},
+	}
+
+	for index, test := range tests {
+		times := 100
+		max, maxF, maxN := test.max, test.max, int64(test.max)
+		min, minF, minN := test.min, test.min, int64(test.min)
+
+		if min > max {
+			max, min = min, max
+		}
+		var rangeF = fmt.Sprintf("range [%v, %v)", min, max)
+		var rangeN = fmt.Sprintf("range [%v, %v)", int64(min), int64(max))
+
+		if max == min {
+			times = 3
+		}
+
+		for n := 0; n < times; n++ {
+			var resultF = RandRangeFloat64(minF, maxF)
+			var msgF = fmt.Sprintf("expecting %v in %v", resultF, rangeF)
+			t.Logf("Test %v.%2d [F]: %v\n", index+1, n, msgF)
+			if max != min {
+				assert.True(t, min <= resultF && resultF < max, msgF)
+			} else {
+				assert.Equal(t, min, resultF, msgF)
+			}
+
+			var resultN = RandRangeInt64(minN, maxN)
+			var msgN = fmt.Sprintf("expecting %v in %v", resultN, rangeN)
+			t.Logf("Test %v.%2d [N]: %v\n", index+1, n, msgN)
+			if int64(max) != int64(min) {
+				assert.True(t, int64(min) <= resultN && resultN < int64(max), msgN)
+			} else {
+				assert.Equal(t, int64(min), resultN, msgN)
+			}
+		}
+		t.Log("")
+	}
+}
+
+// TestSqrt tests Sqrt function
+func TestSqrt(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < 100; i++ {
+		rnd := r.Float64() * 100
+		exp := math.Sqrt(rnd)
+		val := Sqrt(rnd)
+		msg := fmt.Sprintf("expecting Sqrt(%v): %v ~= %v", rnd, val, exp)
+		t.Logf("Test %2d: %v\n", i+1, msg)
+		assert.True(t, NearEqual(exp, val, MathPrecision), msg)
 	}
 }
