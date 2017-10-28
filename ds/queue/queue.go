3b428cedca7a187b238200903476310149277991
Jason Zhu <jason.zhuyx@gmail.com>
2017-10-27 21:15:55 -0700

    added Element and Queue in ds/queue/queue.go

--- ds/queue/queue.go
+++ ds/queue/queue.go
@@ -1,2 +1,23 @@
 // Package queue :: queue.go
 package queue
+
+// Element respresents an item in Queue
+type Element interface{}
+
+// Queue is a slice of Element
+type Queue []Element
+
+// Dequeue remove the first element from queue
+func (q *Queue) Dequeue() Element {
+	if len(*q) > 0 {
+		v := (*q)[0]
+		*q = (*q)[1:]
+		return v
+	}
+	return nil
+}
+
+// Enqueue adds (appends) an element into queue
+func (q *Queue) Enqueue(v Element) {
+	*q = append(*q, v)
+}
