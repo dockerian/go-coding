ab63060c36aad1e446dac7fcf1711aca885f344e
Jason Zhu <jason.zhuyx@gmail.com>
2018-05-31 12:54:15 -0700

    added func GetMergedChannels

--- demo/chan.go
+++ demo/chan.go
@@ -5,6 +5,37 @@ import (
 	u "github.com/dockerian/go-coding/utils"
 )
 
+// GetMergedChannels merges two channels to one.
+// See https://medium.com/justforfunc/why-are-there-nil-channels-in-go-9877cc0b2308
+// Notes: in common pattern, each channel (e.g. `ch` as `chan<- int`)
+//   - receives data in a lengthy producer (e.g. `ch <- v`)
+//   - closes at the end of the producer
+func GetMergedChannels(ch1, ch2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for ch1 != nil || ch2 != nil {
+			select {
+			case v, ok := <-ch1:
+				if !ok { // checking if ch1 is closed
+					ch1 = nil // prevent from busy loop on closed channel which never blocks but nil will
+					// u.Debug("channel ch1 (%v) is closed.\n", &ch1)
+					continue
+				}
+				out <- v
+			case v, ok := <-ch2:
+				if !ok { // checking if ch2 is closed
+					ch2 = nil // prevent from busy loop on closed channel which never blocks but nil will
+					// u.Debug("channel ch2 (%v) is closed.\n", &ch2)
+					continue
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
+}
+
 // MergeChannels merges inputs to one out channel
 func MergeChannels(out chan float32, inputs []chan float32) {
 	var a, b chan float32
